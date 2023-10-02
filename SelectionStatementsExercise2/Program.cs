6ce34cc1fa462509c47b0724d49b2c7c023406fb
David Diggs <ddiggs34@gmail.com>
2023-10-02 14:38:05 -0400

    exercise 2 done

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Diagnostics.Metrics;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
+            Console.WriteLine("What was your favorite subject in school?");
+            string subject = Console.ReadLine();
+
+            switch(subject)
+            {
+                case "English":
+                case "english":
+                    Console.WriteLine("English is fun.");
+                    break;
+                case "Math":
+                case "math":
+                    Console.WriteLine("Math is fun. I am not great with numbers");
+                    break;
+                case "Science":
+                case "science":
+                        Console.WriteLine("I could never get into science");
+                    break;
+                case "Physical education":
+                case "PE":
+                        Console.WriteLine("I love PE!");
+                    break;
+                case "History":
+                case "history":
+                    Console.WriteLine("History is great!");
+                    break;
+
+                default: Console.WriteLine("That's a school subject?! Never had that");
+                    break;
+
+
+            }
         }
     }
 }
